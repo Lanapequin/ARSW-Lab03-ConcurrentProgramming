7d88da35b73da323b1c9d2915e849296dfabf593
Laura Natalia Perilla Quintero <laura.perilla-q@mail.escuelaing.edu.co>
2025-08-29 09:17:45 -0500

    feat: implement wait/notify for producer-consumer and optimize CPU usage based on jVisualVM analysis

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -5,30 +5,36 @@
  */
 package edu.eci.arst.concprg.prodcons;
 
-import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author hcadavid
  */
 public class Consumer extends Thread{
+    Logger logger = Logger.getLogger(getClass().getName());
+
     
-    private Queue<Integer> queue;
+    private final LinkedBlockingQueue<Integer> queue;
     
     
-    public Consumer(Queue<Integer> queue){
-        this.queue=queue;        
+    public Consumer(LinkedBlockingQueue<Integer> queue){
+        this.queue = queue;
     }
     
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+            int elem = 0;
+            try {
+                elem = queue.take();
+            } catch (InterruptedException e) {
+                logger.log(Level.WARNING, "Interrupted!", e);
+                Thread.currentThread().interrupt();
             }
-            
+            logger.log(Level.INFO, "Consumer consumes {0}", elem);
         }
     }
 }
